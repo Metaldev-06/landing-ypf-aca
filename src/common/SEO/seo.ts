b9a9b374e0cd5a38fe123f84ed85e05c42f391ba
Metaldev <fernydiaz62@gmail.com>
2024-05-03 05:14:32 -0300

    Add: SEO

---
+++ src/common/SEO/seo.ts
@@ -0,0 +1,19 @@
+export const seoAstro = {
+  title: "Diaz Servicios SRL",
+  description:
+    "¡Ahorra tiempo y dinero en tu próxima parada! Combustibles de alta calidad, atención amigable y servicios convenientes, todo en un solo lugar.",
+  ogImage:
+    "https://res.cloudinary.com/dw24usdw9/image/upload/v1714722723/kmgmz0ntroqpv0nynkaq.png",
+  ogUrl: "https://www.diazservicios.com.ar/",
+  twitterUrl: "https://www.diazservicios.com.ar/",
+  twitterHandle: "@MetalDev_06",
+  twitterCard: "summary_large_image",
+  twitterImage:
+    "https://res.cloudinary.com/dw24usdw9/image/upload/v1714722723/kmgmz0ntroqpv0nynkaq.png",
+  twitterCreator: "@MetalDev_06",
+  twitterSite: "@MetalDev_06",
+  twitterTitle: "Diaz Servicios SRL",
+  twitterDescription:
+    "¡Ahorra tiempo y dinero en tu próxima parada! Combustibles de alta calidad, atención amigable y servicios convenientes, todo en un solo lugar.",
+  canonical: "https://www.diazservicios.com.ar/",
+};
